37a3c50c5b8ac73e4c82bba0ad599b1e124a7f92
Saleh Mir Mohammad Rezaei <salehmmrezaei@gmail.com>
2025-11-19 16:59:43 +0100

    simulate_episodes method and simulate_learned_strategy implemented

--- q_learning/qlearning.py
+++ q_learning/qlearning.py
@@ -1,8 +1,6 @@
-import gymnasium as gym
 import numpy as np
-import matplotlib.pyplot as plt
+import gymnasium as gym
 import time
-
 from CartPole.config import seed
 
 
@@ -23,7 +21,7 @@ class QLearning:
         self.QMatrix=np.random.uniform(low=0, high=1, size=(numberOfBins[0],numberOfBins[1],numberOfBins[2],numberOfBins[3],self.actionNumber))
 
 
-    def returnIndexState(self, state):
+    def return_index_state(self, state):
         position= state[0]
         velocity = state[1]
         angle = state[2]
@@ -42,7 +40,79 @@ class QLearning:
         return tuple([indexPosition, indexVelocity, indexAngle, indexAngularVelocity])
 
 
-    def selectAction(self,state,index):
+
+    def select_action(self,state,index):
+        if index<500:
+            return np.random.choice(self.actionNumber)
+        randomNumber = np.random.random()
+        if index>7000:
+            self.epsilon = 0.999 * self.epsilon
+
+        if randomNumber<self.epsilon:
+            return  np.random.choice(self.actionNumber)
+        else:
+            return np.random.choice(np.where(self.QMatrix[self.return_index_state(state)]==np.max(self.QMatrix[self.return_index_state(state)]))[0])
+            # here we need to return the minimum index since it can happen
+            # that there are several identical maximal entries, for example
+            # import numpy as np
+            # a=[0,1,1,0]
+            # np.where(a==np.max(a))
+            # this will return [1,2], but we only need a single index
+            # that is why we need to have np.random.choice(np.where(a==np.max(a))[0])
+            # note that zero has to be added here since np.where() returns a tuple
+
+
+    def simulate_episodes(self):
+        for indexEpisode in range(self.numberEpisodes):
+            rewardsEpisode = []
+            (stateS,_) = self.env.reset()
+            stateS = list(stateS)
+            print("Simulating episode {}".format(indexEpisode))
+            terminalState=False
+            while not terminalState:
+                stateSIndex=self.return_index_state(stateS)
+                actionA = self.select_action(stateS,indexEpisode)
+                (stateSprime, reward, terminalState,_,_) = self.env.step(actionA)
+                rewardsEpisode.append(reward)
+                stateSprime = list(stateSprime)
+                stateSprimeIndex = self.return_index_state(stateSprime)
+                QMaxPrime = np.max(self.QMatrix[stateSprimeIndex])
+                if not terminalState:
+                    error = reward + self.gamma * QMaxPrime - self.QMatrix[stateSIndex + (actionA,)]
+                    self.QMatrix[stateSIndex + (actionA,)] = self.QMatrix[stateSIndex + (actionA,)] + self.alpha * error
+                else:
+                    # in the terminal state, we have QMatrix[stateSprime,actionAprime]=0
+                    error = reward - self.QMatrix[stateSIndex + (actionA,)]
+                    self.QMatrix[stateSIndex + (actionA,)] = self.QMatrix[stateSIndex + (actionA,)] + self.alpha * error
+
+            print("Sum of rewards {}".format(np.sum(rewardsEpisode)))
+            self.sumRewardsEpisode.append(np.sum(rewardsEpisode))
+
+
+
+    def simulate_learned_strategy(self):
+        env1 = gym.make('CartPole-v1', render_mode='human')
+        (currentState, _) = env1.reset()
+        env1.render()
+        timeSteps = 1000
+        # obtained rewards at every time step
+        obtainedRewards = []
+
+        for timeIndex in range(timeSteps):
+            print(timeIndex)
+            # select greedy actions
+            actionInStateS = np.random.choice(np.where(self.QMatrix[self.return_index_state(currentState)] == np.max(
+                self.QMatrix[self.return_index_state(currentState)]))[0])
+            currentState, reward, terminated, truncated, info = env1.step(actionInStateS)
+            obtainedRewards.append(reward)
+            time.sleep(0.05)
+            if terminated:
+                time.sleep(1)
+                break
+        return obtainedRewards, env1
+
+
+
 
 
 
